c1f59cb5898b0599277d3712284f2639454a1345
Marcin Gruchała <margruchala@gmail.com>
2021-09-16 21:56:45 +0200

    login logic

--- app/src/main/java/com/frontend/f1bingo/views/loginactivity/LoginActivity.kt
+++ app/src/main/java/com/frontend/f1bingo/views/loginactivity/LoginActivity.kt
@@ -3,12 +3,31 @@ package com.frontend.f1bingo.views.loginactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.frontend.f1bingo.databinding.ActivityLoginBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        FirebaseApp.initializeApp(applicationContext)
+        auth =  Firebase.auth
+    }
+
+    override fun onStart() {
+        super.onStart()
+        isUserCurrentlySignedIn()
+    }
+
+    private fun isUserCurrentlySignedIn() {
+        auth.currentUser?.let {
+            //TODO
+        }
     }
 }
--- app/src/main/java/com/frontend/f1bingo/views/loginactivity/fragments/LoginFragment.kt
+++ app/src/main/java/com/frontend/f1bingo/views/loginactivity/fragments/LoginFragment.kt
@@ -6,38 +6,72 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.frontend.f1bingo.R
 import com.frontend.f1bingo.databinding.FragmentLoginBinding
 import com.frontend.f1bingo.views.mainactivity.MainActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 private const val TAG = "LALF"
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentLoginBinding.inflate(inflater,container,false)
+        auth = Firebase.auth
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setOnClickListeners(view)
+    }
+
+    private fun setOnClickListeners(view: View) {
         binding.apply {
             btnLogin.setOnClickListener {
-                Intent(activity, MainActivity::class.java).also {
-                    startActivity(it)
-                }
+               setLoginClickListener()
             }
             btnRegister.setOnClickListener {
-                Log.d(TAG, "btnRegister clicked")
-                Navigation.findNavController(view)
-                    .navigate(R.id.action_loginFragment_to_registerFragment)
+                setRegistrationClickListener(view)
             }
         }
+    }
+
+    private fun setLoginClickListener() {
+        val email = binding.etEmail.text.trim().toString()
+        val password = binding.etPassword.text.trim().toString()
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener { authResult ->
+                authResult?.let {
+                    startApp()
+                }
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(activity,"Login failed", Toast.LENGTH_LONG).show()
+                Log.d(TAG, e.message.toString())
+            }
+    }
+
+    private fun setRegistrationClickListener(view: View) {
+        Navigation.findNavController(view)
+            .navigate(R.id.action_loginFragment_to_registerFragment)
+    }
+
+    private fun startApp() {
+        val intent = Intent(activity, MainActivity::class.java).apply {
+            flags = (Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
+        startActivity(intent)
     }
 
 }
