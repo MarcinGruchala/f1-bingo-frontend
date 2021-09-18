91086128021163d50f95e27c3d6e3669b22d6ddc
Marcin Gruchała <margruchala@gmail.com>
2021-09-18 17:54:45 +0200

    Printing basic user data and added sign out functionality

--- app/src/main/java/com/frontend/f1bingo/views/loginactivity/LoginActivity.kt
+++ app/src/main/java/com/frontend/f1bingo/views/loginactivity/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.frontend.f1bingo.views.loginactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.frontend.f1bingo.databinding.ActivityLoginBinding
+import com.frontend.f1bingo.views.mainactivity.MainActivity
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -27,7 +29,14 @@ class LoginActivity : AppCompatActivity() {
 
     private fun isUserCurrentlySignedIn() {
         auth.currentUser?.let {
-            //TODO
+            startApp()
         }
+    }
+
+    private fun startApp() {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = (Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/frontend/f1bingo/views/mainactivity/fragments/ProfileFragment.kt
+++ app/src/main/java/com/frontend/f1bingo/views/mainactivity/fragments/ProfileFragment.kt
@@ -1,20 +1,58 @@
 package com.frontend.f1bingo.views.mainactivity.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.frontend.f1bingo.R
+import androidx.fragment.app.Fragment
+import com.frontend.f1bingo.databinding.FragmentProfileBinding
+import com.frontend.f1bingo.views.loginactivity.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-
+private const val TAG = "MAPF"
 class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        auth = Firebase.auth
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindUserData()
+        setOnClickListeners()
+    }
+
+    private fun bindUserData() {
+        auth.currentUser?.let {
+            binding.tvUserEmail.text = "User email: ${it.email}"
+        }
+    }
+
+    private fun setOnClickListeners() {
+        binding.apply {
+            btnSignOut.setOnClickListener {
+                Log.d(TAG,"Sign out")
+                auth.signOut()
+                startLoginActivity()
+            }
+        }
+    }
+
+    private fun startLoginActivity() {
+        val intent = Intent(activity, LoginActivity::class.java).apply {
+            flags = (Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
+        startActivity(intent)
+    }
 }
